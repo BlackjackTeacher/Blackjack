204f9244c9fce1c4c3c96d70f1c6a25895974808
Toasty <thoreasam@gmail.com>
2024-01-23 14:44:36 +0100

    Model load test

---
+++ main.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement);
+
+camera.position.z = 5;
+
+const loader = new GLTFLoader();
+
+loader.load('Models/PCTest/a_personal_computer.glb', function ( gltf ) {
+
+    scene.add(gltf.scene);
+
+}, undefined, function (error){
+
+    console.error;('Wir haben einen Brief gegriegt, ich will mich umbringen');
+
+});
+
+
+function animate() {
+    requestAnimationFrame( animate );
+    renderer.render(scene, camera);
+}
+animate();
