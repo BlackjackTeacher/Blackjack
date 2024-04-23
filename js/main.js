236bf4e5d2cbd4a7909d2359df7cbc11803d6f9d
Toasty <thoreasam@gmail.com>
2024-04-23 12:28:30 +0200

    Karten und Kartengruppen Variablen

--- js/main.js
+++ js/main.js
@@ -83,7 +83,64 @@ loader.load('Models/Karten/Karte_Deck.glb', (gltf) => {
     Deck.scale.set(7.5, 7.5, 7.5);
     scene.add(Deck);
     
+    // einzel Karteninitialisierung
+    const aceOfClubs = scene.getObjectByName('Card_Ace_Clubs');
+    const twoOfClubs = scene.getObjectByName('Card_Two_Clubs');
+    const threeOfClubs = scene.getObjectByName('Card_Three_Clubs');
+    const fourOfClubs = scene.getObjectByName('Card_Four_Clubs');
+    const fiveOfClubs = scene.getObjectByName('Card_Five_Clubs');
+    const sixOfClubs = scene.getObjectByName('Card_Six_Clubs');
+    const sevenOfClubs = scene.getObjectByName('Card_Seven_Clubs');
+    const eightOfClubs = scene.getObjectByName('Card_Eight_Clubs');
+    const nineOfClubs = scene.getObjectByName('Card_Nine_Clubs');
+    const tenOfClubs = scene.getObjectByName('Card_Ten_Clubs');
+    const jackOfClubs = scene.getObjectByName('Card_Jack_Clubs');
+    const queenOfClubs = scene.getObjectByName('Card_Queen_Clubs');
+    const kingOfClubs = scene.getObjectByName('Card_King_Clubs');
+    const aceOfSpades = scene.getObjectByName('Card_Ace_Spades');
+    const twoOfSpades = scene.getObjectByName('Card_Two_Spades');
+    const threeOfSpades = scene.getObjectByName('Card_Three_Spades');
+    const fourOfSpades = scene.getObjectByName('Card_Four_Spades');
+    const fiveOfSpades = scene.getObjectByName('Card_Five_Spades');
+    const sixOfSpades = scene.getObjectByName('Card_Six_Spades');
+    const sevenOfSpades = scene.getObjectByName('Card_Seven_Spades');
+    const eightOfSpades = scene.getObjectByName('Card_Eight_Spades');
+    const nineOfSpades = scene.getObjectByName('Card_Nine_Spades');
+    const tenOfSpades = scene.getObjectByName('Card_Ten_Spades');
+    const jackOfSpades = scene.getObjectByName('Card_Jack_Spades');
+    const queenOfSpades = scene.getObjectByName('Card_Queen_Spades');
+    const kingOfSpades = scene.getObjectByName('Card_King_Spades');
+    const aceOfHearts = scene.getObjectByName('Card_Ace_Hearts');
+    const twoOfHearts = scene.getObjectByName('Card_Two_Hearts');
+    const threeOfHearts = scene.getObjectByName('Card_Three_Hearts');
+    const fourOfHearts = scene.getObjectByName('Card_Four_Hearts');
+    const fiveOfHearts = scene.getObjectByName('Card_Five_Hearts');
+    const sixOfHearts = scene.getObjectByName('Card_Six_Hearts');
+    const sevenOfHearts = scene.getObjectByName('Card_Seven_Hearts');
+    const eightOfHearts = scene.getObjectByName('Card_Eight_Hearts');
+    const nineOfHearts = scene.getObjectByName('Card_Nine_Hearts');
+    const tenOfHearts = scene.getObjectByName('Card_Ten_Hearts');
+    const jackOfHearts = scene.getObjectByName('Card_Jack_Hearts');
+    const queenOfHearts = scene.getObjectByName('Card_Queen_Hearts');
+    const kingOfHearts = scene.getObjectByName('Card_King_Hearts');
+    const aceOfDiamonds = scene.getObjectByName('Card_Ace_Diamonds');
+    const twoOfDiamonds = scene.getObjectByName('Card_Two_Diamonds');
+    const threeOfDiamonds = scene.getObjectByName('Card_Three_Diamonds');
+    const fourOfDiamonds = scene.getObjectByName('Card_Four_Diamonds');
+    const fiveOfDiamonds = scene.getObjectByName('Card_Five_Diamonds');
+    const sixOfDiamonds = scene.getObjectByName('Card_Six_Diamonds');
+    const sevenOfDiamonds = scene.getObjectByName('Card_Seven_Diamonds');
+    const eightOfDiamonds = scene.getObjectByName('Card_Eight_Diamonds');
+    const nineOfDiamonds = scene.getObjectByName('Card_Nine_Diamonds');
+    const tenOfDiamonds = scene.getObjectByName('Card_Ten_Diamonds');
+    const jackOfDiamonds = scene.getObjectByName('Card_Jack_Diamonds');
+    const queenOfDiamonds = scene.getObjectByName('Card_Queen_Diamonds');
+    const kingOfDiamonds = scene.getObjectByName('Card_King_Diamonds');
 
+    clubsGroup.add(aceOfClubs, twoOfClubs, threeOfClubs, fourOfClubs, fiveOfClubs, sixOfClubs, sevenOfClubs, eightOfClubs, nineOfClubs, tenOfClubs, jackOfClubs, queenOfClubs, kingOfClubs)
+    spadesGroup.add(aceOfSpades, twoOfSpades, threeOfSpades, fourOfSpades, fiveOfSpades, sixOfSpades, sevenOfSpades, eightOfSpades, nineOfSpades, tenOfSpades, jackOfSpades, queenOfSpades, kingOfSpades)
+    heartsGroup.add(aceOfHearts, twoOfHearts, threeOfHearts, fourOfHearts, fiveOfHearts, sixOfHearts, sevenOfHearts, eightOfHearts, nineOfHearts, tenOfHearts, jackOfHearts, queenOfHearts, kingOfHearts)
+    diamondsGroup.add(aceOfDiamonds, twoOfDiamonds, threeOfDiamonds, fourOfDiamonds, fiveOfDiamonds, sixOfDiamonds, sevenOfDiamonds, eightOfDiamonds, nineOfDiamonds, tenOfDiamonds, jackOfDiamonds, queenOfDiamonds, kingOfDiamonds)
 
 }, undefined, function (error) {
 
